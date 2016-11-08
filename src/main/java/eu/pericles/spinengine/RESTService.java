c6dc429ae36a5dfc271c61dc007d7d2e1bff2b5b
Fabio Corubolo <corubolo@gmail.com>
2016-11-08 12:32:02 +0100

    Added more documentation, CLI interface.

--- src/main/java/eu/pericles/spinengine/RESTService.java
+++ src/main/java/eu/pericles/spinengine/RESTService.java
@@ -1,6 +1,8 @@
 package eu.pericles.spinengine;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.compose.MultiUnion;
 import org.apache.jena.ontology.OntModel;
@@ -9,6 +11,7 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileUtils;
 import org.apache.jena.vocabulary.RDFS;
+import org.jboss.resteasy.annotations.cache.NoCache;
 import org.topbraid.spin.constraints.ConstraintViolation;
 import org.topbraid.spin.constraints.SPINConstraints;
 import org.topbraid.spin.inference.SPINInferences;
@@ -24,120 +27,162 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * A restful web service that implements calls to the SPIN rule engine. It implements methods for rule inference and for constraints checking.
+ * Contains also a command line interface.
+ */
 @Provider
 @Path("/")
 public class RESTService {
 
-    public class SPINResults{
+    public static class CLIParams {
 
+        @Parameter(names = {"-baseURI", "-URI"}, description = "The base URI for SPIN rule and models", required = true)
+        public String uri;
 
-        public String constraints;
+        @Parameter(names = "-basedoc", description = "The actual document in case it's not accessible or available at the specified URI")
+        public String baseDoc;
 
-        public SPINResults(String constraints, String model, String newTriplets, String consoleLog) {
-            this.constraints = constraints;
-            this.model = model;
-            this.newTriplets = newTriplets;
-            this.consoleLog = consoleLog;
-        }
+        @Parameter(names = "-SPINURI", description = "The URI containing the SPIN rules, if separate and not imported by the base model")
+        public String SPINURI;
 
-        public String model = "";
-
-        public String newTriplets="";
-        public String consoleLog="";
-
-
-        public String getConstraints() {
-            return constraints;
-        }
-
-        public String getModel() {
-            return model;
-        }
-
-        public String getNewTriplets() {
-            return newTriplets;
-        }
-
-        public String getConsoleLog() {
-            return consoleLog;
-        }
-    }
-
-    public class SPINResultsJson extends SPINResults{
-
-        public SPINResultsJson(String constraints, String model, String newTriplets, String consoleLog, String jsonModel, String jsonNewTriplets, String jsonConstraints) {
-            super(constraints, model, newTriplets, consoleLog);
-            this.jsonModel = jsonModel;
-            this.jsonNewTriplets = jsonNewTriplets;
-            this.jsonConstraints = jsonConstraints;
-        }
-
-        @JsonRawValue
-        public String jsonModel = "";
-        @JsonRawValue
-        public String jsonNewTriplets="";
-        @JsonRawValue
-        public String jsonConstraints;
+        @Parameter(names = "-SPINdoc", description = "The actual SPIN rule document, in case it's not accessible or available at the specified URI")
+        public String SPINdoc;
+        @Parameter(names = "-outFormat", description = "The result output format, default TTL.  Options are TTL, NTRIPLES, RDFXML, N3, JSONLD, RDFJSON. For the complete list, see formats defined in the JENA API: https://jena.apache.org/documentation/io/rdf-output.html")
+        public String outFormat = "TTL";
+        @Parameter(names = "--help", description = "Will show this message", help = true)
+        public boolean help;
+        @Parameter(names = "--constraints", description = "Will check constraints running inference")
+        public boolean constraints = false;
+        @Parameter(names = "--constraints-no-inference", description = "Will check constraints without running inference")
+        public boolean constraintsnoinf = false;
 
     }
-    @GET
-    @Path("/test")
-    @Produces("text/plain")
-    public String test() {
-        return "test";
+
+    public static void main(String[] args) {
+        CLIParams p = new CLIParams();
+
+        try {
+            JCommander c = new JCommander(p, args);
+            if (p.help) {
+                c.usage();
+            }
+
+        } catch (ParameterException x) {
+            System.err.println(x.getMessage());
+            new JCommander(p).usage();
+            return;
+        }
+
+        SPINResults s = runInference(p.uri, p.baseDoc, p.SPINURI, p.SPINdoc, p.outFormat, p.constraints || p.constraintsnoinf, !p.constraintsnoinf);
+        System.out.print(s.toString().replace("\\n", "\\n\n"));
+
     }
 
 
     /**
+     * Simple test method
      *
-     * @param baseURI
-     * @param document
-     * @param SPINURI
-     * @param SPINdocument
-     * @param outFormat see formats defined in the JENA API: https://jena.apache.org/documentation/io/rdf-output.html
-     * @return
+     * @return the 'test at ' String + current date
+     */
+    @GET
+    @Path("/test")
+    @Produces("text/plain")
+    public String test() {
+        return "test at " + LocalDateTime.now();
+    }
+
+
+    /**
+     * This POST method call will execute the rule engine inference on the specified data. The required parameter is the base URI,
+     * that will be used to gather the rules and models.
+     *
+     * @param baseURI      The base URI for SPIN rule and models.
+     * @param document     The actual document in case it's not accessible or available at the specified URI
+     * @param SPINURI      The URI containing the SPIN rules, if separate and not imported by the base model
+     * @param SPINdocument The actual SPIN rule document, in case it's not accessible or available at the specified URI
+     * @param outFormat    the result output format, default TTL.  Options are TTL, NTRIPLES, RDFXML, N3, JSONLD, RDFJSON. For the complete list, see formats defined in the JENA API: https://jena.apache.org/documentation/io/rdf-output.html
+     * @return a JSON result set in the form of a {@link SPINResults} format
      */
     @POST
     @Path("/runInferencesPost")
     @Consumes("application/x-www-form-urlencoded")
-    @Produces("text/plain")
-    public SPINResults runInferences(@FormParam("baseURI") final String baseURI, @FormParam("document") final String document, @FormParam("SPINURI") final String SPINURI, @FormParam("SPINdocument") final String SPINdocument, @FormParam("outFormat") final String outFormat) {
-        return runInference(baseURI, document,SPINURI, SPINdocument, outFormat, false, true);
+    @Produces("application/json")
+    @NoCache()
+    public SPINResults runInferences(@FormParam("baseURI") String baseURI, @FormParam("document") String document, @FormParam("SPINURI") String SPINURI, @FormParam("SPINdocument") String SPINdocument, @FormParam("outFormat") String outFormat) {
+        return runInference(baseURI, document, SPINURI, SPINdocument, outFormat, false, true);
     }
 
+    /**
+     * This GET method call will execute the rule engine inference on the specified data. The required parameter is the base URI,
+     * that will be used to gather the rules and models.
+     *
+     * @param baseURI      The base URI for SPIN rule and models.
+     * @param document     The actual document in case it's not accessible or available at the specified URI
+     * @param SPINURI      The URI containing the SPIN rules, if separate and not imported by the base model
+     * @param SPINdocument The actual SPIN rule document, in case it's not accessible or available at the specified URI
+     * @param outFormat    the result output format, default TTL.  Options are TTL, NTRIPLES, RDFXML, N3, JSONLD, RDFJSON. For the complete list, see formats defined in the JENA API: https://jena.apache.org/documentation/io/rdf-output.html
+     * @return a JSON result set in the form of a {@link SPINResults} format
+     */
     @GET
     @Path("/runInferencesGet")
     @Produces("application/json")
-    public SPINResults runInferencesGet(@QueryParam("baseURI") final String baseURI, @QueryParam("document") final String document,@QueryParam("SPINURI") final String SPINURI, @QueryParam("SPINdocument") final String SPINdocument, @QueryParam("outFormat")final String outFormat) {
+    @NoCache()
+    public SPINResults runInferencesGet(@QueryParam("baseURI") String baseURI, @QueryParam("document") String document, @QueryParam("SPINURI") String SPINURI, @QueryParam("SPINdocument") String SPINdocument, @QueryParam("outFormat") String outFormat) {
 
-        return runInference(baseURI, document,SPINURI, SPINdocument, outFormat, false, true);
+        return runInference(baseURI, document, SPINURI, SPINdocument, outFormat, false, true);
     }
 
 
-
-
+    /**
+     * This POST method call will execute the rule engine constraints (optionally inference as well) on the specified data. The required parameter is the base URI,
+     * that will be used to gather the rules and models.
+     *
+     * @param baseURI      The base URI for SPIN rule and models.
+     * @param document     The actual document in case it's not accessible or available at the specified URI
+     * @param SPINURI      The URI containing the SPIN rules, if separate and not imported by the base model
+     * @param SPINdocument The actual SPIN rule document, in case it's not accessible or available at the specified URI
+     * @param outFormat    the result output format, default TTL.  Options are TTL, NTRIPLES, RDFXML, N3, JSONLD, RDFJSON. For the complete list, see formats defined in the JENA API: https://jena.apache.org/documentation/io/rdf-output.html
+     * @param doInference  perform inference before constraint checking. Defaults true
+     * @return a JSON result set in the form of a {@link SPINResults} format
+     */
     @POST
     @Path("/runConstraintsPost")
     @Consumes("application/x-www-form-urlencoded")
     @Produces("application/json")
-    public SPINResults runConstraintsPost(@FormParam("baseURI") final String baseURI, @FormParam("document") final String document, @FormParam("SPINURI") final String SPINURI, @FormParam("SPINdocument") final String SPINdocument, @QueryParam("outFormat")final String outFormat, @FormParam("doInference") final boolean doInference) {
-        return runInference(baseURI, document,SPINURI, SPINdocument ,outFormat, true, doInference);
+    @NoCache()
+    public SPINResults runConstraintsPost(@FormParam("baseURI") String baseURI, @FormParam("document") String document, @FormParam("SPINURI") String SPINURI, @FormParam("SPINdocument") String SPINdocument, @QueryParam("outFormat") String outFormat, @FormParam("doInference") @DefaultValue("true") boolean doInference) {
+        return runInference(baseURI, document, SPINURI, SPINdocument, outFormat, true, doInference);
     }
 
+    /**
+     * This GET method call will execute the rule engine constraints (optionally inference as well) on the specified data. The required parameter is the base URI,
+     * that will be used to gather the rules and models.
+     *
+     * @param baseURI      The base URI for SPIN rule and models.
+     * @param document     The actual document in case it's not accessible or available at the specified URI
+     * @param SPINURI      The URI containing the SPIN rules, if separate and not imported by the base model
+     * @param SPINdocument The actual SPIN rule document, in case it's not accessible or available at the specified URI
+     * @param doInference  perform inference before constraint checking. Defaults true
+     * @param outFormat    the result output format, default TTL.  Options are TTL, NTRIPLES, RDFXML, N3, JSONLD, RDFJSON. For the complete list, see formats defined in the JENA API: https://jena.apache.org/documentation/io/rdf-output.html
+     * @return a JSON result set in the form of a {@link SPINResults} format
+     */
     @GET
     @Path("/runConstraintsGet")
     @Produces("application/json")
-    public SPINResults runConstraintsGet(@QueryParam("baseURI") final String baseURI, @QueryParam("document") final String document,@QueryParam("SPINURI") final String SPINURI, @QueryParam("SPINdocument") final String SPINdocument,  @QueryParam("outFormat")final String outFormat, @QueryParam("doInference") final boolean doInference) {
-        return runInference(baseURI, document,SPINURI, SPINdocument ,outFormat, true, doInference);
+    @NoCache()
+    public SPINResults runConstraintsGet(@QueryParam("baseURI") String baseURI, @QueryParam("document") String document, @QueryParam("SPINURI") String SPINURI, @QueryParam("SPINdocument") String SPINdocument, @QueryParam("outFormat") String outFormat, @QueryParam("doInference") @DefaultValue("true") boolean doInference) {
+        return runInference(baseURI, document, SPINURI, SPINdocument, outFormat, true, doInference);
     }
 
-    private SPINResults runInference(String baseURI, String document, String SPINURI, String SPINdocument, String outFormat, boolean checkConstraints, boolean doInference) {
+    private static SPINResults runInference(String baseURI, String document, String SPINURI, String SPINdocument, String outFormat, boolean checkConstraints, boolean doInference) {
         String constraints = "";
         String model = "";
-        String newTriplets="";
-        StringWriter w = new StringWriter();
+        String newTriplets = "";
+        StringWriter w;
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
@@ -145,7 +190,7 @@ public class RESTService {
         PrintStream old = System.out;
         PrintStream olderr = System.err;
 
-        // Tell Java to use your special stream
+        // Tell Java to use your special streams
         System.setOut(ps);
         System.setErr(ps);
         // Put things back
@@ -154,29 +199,27 @@ public class RESTService {
             System.err.flush();
             System.setOut(old);
             System.setErr(olderr);
-
-            return new SPINResults(null,null,null,"The baseURI parameter must be set");
+            return new SPINResults(null, null, null, "The baseURI parameter must be set", null, null, null);
         }
 
-        if (outFormat==null || outFormat.trim().equals("")) {
-            outFormat="TTL";
+        if (outFormat == null || outFormat.trim().equals("")) {
+            outFormat = "TTL";
         }
         // Initialize system functions and templates
-        Model baseModel = null;
+        Model baseModel;
 
         try {
             baseModel = getOntModel(baseURI, document, SPINURI, SPINdocument);
         } catch (org.apache.jena.atlas.web.HttpException x) {
             ps.println(x.getMessage());
             ps.println(baseURI);
-
             System.out.flush();
             System.err.flush();
             System.setOut(old);
             System.setErr(olderr);
-
-            return new SPINResults(null,null,null,baos.toString());
+            return new SPINResults(null, null, null, baos.toString(), null, null, null);
         }
+
         OntModel ontModel = JenaUtil.createOntologyModel(OntModelSpec.OWL_MEM, baseModel);
         // Register locally defined functions
         SPINModuleRegistry.get().registerAll(ontModel, null);
@@ -185,7 +228,7 @@ public class RESTService {
         Model newTriples = ModelFactory.createDefaultModel();
         newTriples.setNsPrefixes(ontModel);
         ontModel.addSubModel(newTriples);
-        List<ConstraintViolation> cvs = null;
+        List<ConstraintViolation> cvs;
         if (doInference) {
             SPINInferences.run(ontModel, newTriples, null, null, false, null);
             // Perform inferencing
@@ -217,35 +260,36 @@ public class RESTService {
         System.setErr(olderr);
 
         if (outFormat.toLowerCase().contains("json"))
-            return new SPINResultsJson("","","", baos.toString(), model, newTriplets,constraints);
+            return new SPINResults("", "", "", baos.toString(), model, newTriplets, constraints);
         else
-            return new SPINResults(constraints, model, newTriplets, baos.toString());
+            return new SPINResults(constraints, model, newTriplets, baos.toString(), null, null, null);
     }
 
-    private Model getOntModel(String baseURI, String document, String SPINURI, String SPINdocument) {
+    /**
+     * Retrieve and builds the ontology model
+     */
+    private static Model getOntModel(String baseURI, String document, String SPINURI, String SPINdocument) {
 
         SPINModuleRegistry.get().init();
         Model baseModel = ModelFactory.createDefaultModel();
         String lang = FileUtils.guessLang(baseURI);
         if (document != null) {
             baseModel.read(new StringReader(document), baseURI, lang);
-        }
-        else {
+        } else {
             baseModel.read(baseURI, lang);
         }
-        if (SPINURI!=null) {
+        if (SPINURI != null) {
             Model spiModel = ModelFactory.createDefaultModel();
 
             String lang2 = FileUtils.guessLang(SPINURI);
 
             if (document != null) {
                 spiModel.read(new StringReader(SPINdocument), SPINURI, lang2);
-            }
-            else {
+            } else {
                 spiModel.read(SPINURI, lang);
             }
             // Create Model for the base graph with its imports
-            MultiUnion union = new MultiUnion(new Graph[] {
+            MultiUnion union = new MultiUnion(new Graph[]{
                     baseModel.getGraph(),
                     spiModel.getGraph(),
                     SPL.getModel().getGraph(),
---
+++ src/main/java/eu/pericles/spinengine/SPINResults.java
@@ -0,0 +1,94 @@
+package eu.pericles.spinengine;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * This class represents the results of the rule engine execution, consisting in the models and console logs generated
+ * by the rule engine library. Some of the results may be empty depending on the system call.
+ */
+public class SPINResults {
+
+    /**
+     * @param constraints     Defined if the output is NOT in JSON format. The model results for constraint checking
+     * @param model           Defined if the output is NOT in JSON format. The model union model (modified original model)
+     * @param newTriplets     Defined if the output is NOT in JSON format. The new generated triples.
+     * @param consoleLog      The results of the console log
+     * @param jsonModel       If Json output specified:  The model union model (modified original model)
+     * @param jsonNewTriplets If Json output specified:  The new generated triples.
+     * @param jsonConstraints If Json output specified:   The model results for constraint checking
+     */
+    public SPINResults(String constraints, String model, String newTriplets, String consoleLog, String jsonModel, String jsonNewTriplets, String jsonConstraints) {
+        this.constraints = constraints;
+        this.model = model;
+        this.newTriplets = newTriplets;
+        this.consoleLog = consoleLog;
+        this.jsonModel = jsonModel;
+        this.jsonNewTriplets = jsonNewTriplets;
+        this.jsonConstraints = jsonConstraints;
+    }
+
+    /**
+     * Defined if the output is NOT in JSON format. The model union model (modified original model)
+     */
+    public String model = "";
+    /**
+     * Defined if the output is NOT in JSON format. The model results for constraint checking
+     */
+    public String constraints;
+    /**
+     * Defined if the output is NOT in JSON format. The new generated triples.
+     */
+    public String newTriplets = "";
+    /**
+     * The results of the console log
+     */
+    public String consoleLog = "";
+
+    /**
+     * If Json output specified:  The model union model (modified original model)
+     */
+    @JsonRawValue
+    public String jsonModel = "";
+    /**
+     * If Json output specified:  The new generated triples.
+     */
+    @JsonRawValue
+    public String jsonNewTriplets = "";
+    /**
+     * If Json output specified:   The model results for constraint checking
+     */
+    @JsonRawValue
+    public String jsonConstraints;
+
+
+    public String getConstraints() {
+        return constraints;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getNewTriplets() {
+        return newTriplets;
+    }
+
+    public String getConsoleLog() {
+        return consoleLog;
+    }
+
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        //Object to JSON in String
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+}
